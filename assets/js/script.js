b49a93b053475b110c1aacea01086a8bba944c1b
Corey Motte <coreydavidmotte@gmail.com>
2022-03-06 16:51:54 -0500

    add functionality for save button, saves text per hour to localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,22 @@ $(document).ready(function () {
     $("#currentDay").text(currentTime);
 
     // Function for when a save button is clicked, save text data to localStorage
-    
+    $(".saveBtn").click(function (){
+        taskText = $(this).siblings("textarea").val();
+        taskTime = $(this).siblings("textarea").attr("id");
+
+        task = {
+            time: taskTime,
+            text: taskText
+        }
+
+        temp = JSON.parse(localStorage.getItem('tasks'));
+        if (temp === null) {
+            localStorage.setItem('tasks', JSON.stringify([{ time: taskTime, text: taskText}]));
+        } else {
+            temp.push(task);
+            localStorage.setItem('tasks', JSON.stringify(temp));
+        }
+    });
 
 });
