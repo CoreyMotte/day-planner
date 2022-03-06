8a9308ec940f6e797368312ce37b89780e265dba
Corey Motte <coreydavidmotte@gmail.com>
2022-03-06 16:43:34 -0500

    display current date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+
+    let currentTime = moment().format("dddd [-] LL");
+    $("#currentDay").text(currentTime);
+
+    // Function for when a save button is clicked, save text data to localStorage
     
 
 });
